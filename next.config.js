c11e90b00659c340e106bc001f8a2bfc34c1a7d7
luiseg2710 <72046701+luiseg2710@users.noreply.github.com>
2022-12-06 00:34:47 -0300

    Update next.config.js
    -fixing the next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
   images: {
-    protocol: 'https',
-    hostname: 'images.pexels.com',
-    port: 'http://localhost:3000/',
-    pathname: '../assets/images/condo.jpg'
-  }
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '3000',
+        pathname: '/photos/87223/pexels-photo-87223.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      },
+    ],
+  },
 }
-
-module.exports = nextConfig
